47903f1ae559a2890a6fd0c0db98b01b28e90af6
Bryan Chachere <bryan258@gmail.com>
2025-07-13 14:01:20 -0700

    End of Section 4.

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,14 +1,12 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+
+    public class MembersController(AppDbContext context) : BaseApiController
     {
 
         [HttpGet]
@@ -17,6 +15,7 @@ namespace API.Controllers
             var members = await context.Users.ToListAsync();
             return Ok(members);
         }
+
 
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetMember(string id)
